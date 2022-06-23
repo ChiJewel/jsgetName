eb3efc0abc0aa064872d73a4c52136b2810225e6
ChiJewel <105532906+ChiJewel@users.noreply.github.com>
2022-06-23 01:27:56 -0700

    Create getUser.js

---
+++ getUser.js
@@ -0,0 +1,7 @@
+var nam = window. prompt("Enter your name: ");
+var height = window. prompt("Enter your height: ");
+var country = window. prompt("Enter your country: ");
+
+
+alert("welcome Mr: " + nam, "your height is: " + height, "and your country is: " + country, "welcome we are pleased to have you.")
+
